8d22bbc56c16460d6526ee6e764473e894d6b98c
liuha <liuhanqimark@outlook.com>
2025-03-18 22:22:20 -0400

    add README and header of cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: Hanqi Liu
+* Assignment: Lunar Lander
+* Date due: 2025-3-15, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 
---
+++ README.md
@@ -0,0 +1,4 @@
+This a magic version of lunar lander game, control your magic fireball to hit a slime!
+Thoes slimes are super clever, they are running away from your screen. DON LET THEM ESCAPE!!
+BTW, I am a freshman magic student, so it is hard control my fireball :(
+GOOD LUCK!
